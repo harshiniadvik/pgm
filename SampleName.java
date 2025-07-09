aab917ab207158ae1b747f25cf725cdc0b2e1f4e
harshiniadvik <v.harshini1817@gmail.com>
2025-07-09 22:32:08 +0530

    Update SampleName.java

--- SampleName.java
+++ SampleName.java
@@ -1,7 +1,7 @@
 public class SampleName {
     public static void main(String[] args) {
         // Declare a variable to hold the name
-        String name = "adviaadhyasri";
+        String name = "adviaadhyasr1i";
         
         // Print the name to the console
         System.out.println("Hello, my name is " + name);
