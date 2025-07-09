1a9c6c4ff3a607011842ca363758f0a4e24d392e
harshini <harshini>
2025-07-09 19:23:44 +0530

    sdfsg

---
+++ SampleName.java
@@ -0,0 +1,9 @@
+public class SampleName {
+    public static void main(String[] args) {
+        // Declare a variable to hold the name
+        String name = "John Doe";
+        
+        // Print the name to the console
+        System.out.println("Hello, my name is " + name);
+    }
+}
--- pgm.java
+++
@@ -1,5 +0,0 @@
-public class sample{
-    public static void(string[] args)
-    system.out.print("harshinikondapaka")
-}
-}
